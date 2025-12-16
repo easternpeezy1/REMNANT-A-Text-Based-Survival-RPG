f36c426bb61455327f257a013b62d74770e6b51f
easternpeezy1 <easternpeezy@gmail.com>
2025-12-15 19:54:43 -0600

    Fixed module loading and refactored game structure

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  win.loadFile('index.html');
+  win.webContents.openDevTools(); 
+  
+  // Remove menu bar (optional)
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
